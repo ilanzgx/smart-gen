f011f2565e000a798c6f66a5b4f8012d36dc95c3
ilanzgx <ilan-_@hotmail.com>
2026-04-12 01:20:49 -0300

    ci(website): configura pipeline de deploy na vercel

--- apps/website/src/lib/supabase.ts
+++ apps/website/src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createSupabaseInstance } from '@smart-gen/supabase'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('As variáveis de ambiente do Supabase não foram configuradas!')
