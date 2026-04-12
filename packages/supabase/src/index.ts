6001e572965bc8304ae020ca3b6bee4aff2dba84
ilanzgx <ilan-_@hotmail.com>
2026-04-12 16:16:24 -0300

    fix(supabase): ajuste para retornar todos os dados e interfaces

--- packages/supabase/src/index.ts
+++ packages/supabase/src/index.ts
@@ -3,3 +3,5 @@ export type { SupabaseClient, User, Session } from "@supabase/supabase-js";
 
 export * from "./resources/generators";
 export * from "./resources/auth";
+export * from "./resources/readings";
+export * from "./resources/users";
