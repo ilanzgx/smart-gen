c2f0b76a46be6832297dfe39d030c0b13a52a614
ilanzgx <ilan-_@hotmail.com>
2026-04-12 14:07:44 -0300

    feat(supabase): criar funções restantes da extração de dados das tabelas de registros/leituras e usuários

---
+++ packages/supabase/src/resources/readings/queries.ts
@@ -0,0 +1,59 @@
+import type { SupabaseClient } from "@supabase/supabase-js";
+import type { Database } from "../../database.types";
+import type { Leitura } from "./types";
+
+/**
+ * Retorna todas as leituras.
+ * @param {SupabaseClient} supabase - Instância do Supabase.
+ * @returns {Promise<Leitura[]>} - Dados de todas as leituras.
+ */
+export const getReadings = async (
+  supabase: SupabaseClient<Database>,
+): Promise<Leitura[]> => {
+  const { data, error } = await supabase.from("registro").select("*");
+
+  if (error) throw error;
+
+  return data as Leitura[];
+};
+
+/**
+ * Retorna uma leitura pelo ID.
+ * @param {SupabaseClient} supabase - Instância do Supabase.
+ * @param {string} id - ID da leitura.
+ * @returns {Promise<Leitura>} - Dados da leitura.
+ */
+export const getReadingById = async (
+  supabase: SupabaseClient<Database>,
+  id: string,
+): Promise<Leitura> => {
+  const { data, error } = await supabase
+    .from("registro")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  if (error) throw error;
+
+  return data as Leitura;
+};
+
+/**
+ * Retorna todas as leituras de um gerador.
+ * @param {SupabaseClient} supabase - Instância do Supabase.
+ * @param {string} generatorId - ID do gerador.
+ * @returns {Promise<Leitura[]>} - Dados de todas as leituras do gerador.
+ */
+export const getReadingsByGeneratorId = async (
+  supabase: SupabaseClient<Database>,
+  generatorId: string,
+): Promise<Leitura[]> => {
+  const { data, error } = await supabase
+    .from("registro")
+    .select("*")
+    .eq("gerador_id", generatorId);
+
+  if (error) throw error;
+
+  return data as Leitura[];
+};
--- packages/supabase/src/resources/users/queries.ts
+++ packages/supabase/src/resources/users/queries.ts
@@ -1,1 +1,60 @@
-// Operações de leitura (select)
+import type { SupabaseClient } from "@supabase/supabase-js";
+import type { Database } from "../../database.types";
+import type { UserProfile } from "./types";
+
+/**
+ * Retorna todos os usuários.
+ * @param {SupabaseClient} supabase - Instância de SupabaseClient.
+ * @returns {Promise<UserProfile[]>} - Dados dos usuários.
+ */
+export const getUsers = async (
+  supabase: SupabaseClient<Database>,
+): Promise<UserProfile[]> => {
+  const { data, error } = await supabase.from("usuario").select("*");
+
+  if (error) throw error;
+
+  return data as UserProfile[];
+};
+
+/**
+ * Retorna os dados de um usuário específico pelo ID.
+ * @param {SupabaseClient} supabase - Instância de SupabaseClient.
+ * @param {string} id - ID do usuário.
+ * @returns {Promise<UserProfile>} - Dados do usuário.
+ */
+export const getUserById = async (
+  supabase: SupabaseClient<Database>,
+  id: string,
+): Promise<UserProfile> => {
+  const { data, error } = await supabase
+    .from("usuario")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  if (error) throw error;
+
+  return data as UserProfile;
+};
+
+/**
+ * Retorna os dados de um usuário específico pelo email.
+ * @param {SupabaseClient} supabase - Instância de SupabaseClient.
+ * @param {string} email - Email do usuário.
+ * @returns {Promise<UserProfile>} - Dados do usuário.
+ */
+export const getUserByEmail = async (
+  supabase: SupabaseClient<Database>,
+  email: string,
+): Promise<UserProfile> => {
+  const { data, error } = await supabase
+    .from("usuario")
+    .select("*")
+    .eq("email", email)
+    .single();
+
+  if (error) throw error;
+
+  return data as UserProfile;
+};
