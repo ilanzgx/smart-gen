15aade204cd2adec3c8b818724c11df2e4c3ad5c
GabrielVerwt <ryang6361@gmail.com>
2026-04-15 13:17:45 -0300

    feat(website): exibir usuario e função de logout

--- apps/website/src/views/DashboardView.vue
+++ apps/website/src/views/DashboardView.vue
@@ -1,7 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabase';
+import { useAuthStore } from '@/stores/auth.store';
+import { getUser, type User } from '@smart-gen/supabase';
+import { onMounted, ref } from 'vue';
+import { RouterLink } from 'vue-router';
+
+const name = ref<User | null>(null);
+
+function logout() {
+  useAuthStore().signOut();
+}
+
+onMounted(async () => {
+  name.value = (await getUser(supabase)).user_metadata?.name;
+});
+</script>
 
 <template>
-  <div>
-    <h1>Dashboard View</h1>
-  </div>
+  <main class="flex min-h-screen items-center justify-center p-4">
+    <div class="w-full max-w-sm space-y-6">
+      <div class="space-y-2 text-center">
+        <h1 class="text-2xl font-bold tracking-tight">👋 Olá, {{ name }}</h1>
+      </div>
+
+      <div class="space-y-2 text-center">
+        <RouterLink to="/">
+          <button @click="logout" class="text-blue-600 hover:underline">Logout</button>
+        </RouterLink>
+      </div>
+    </div>
+  </main>
+
 </template>
+
