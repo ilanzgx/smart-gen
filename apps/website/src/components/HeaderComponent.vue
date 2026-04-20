ece72977cabc0d0b5983a1a824db23c50ec6686e
Gustavo Lisboa <gustaedjeff@gmail.com>
2026-04-20 09:04:46 -0300

    feat(website): modificar funcionalidades do Header

--- apps/website/src/components/HeaderComponent.vue
+++ apps/website/src/components/HeaderComponent.vue
@@ -3,29 +3,32 @@ import { useAuthStore } from '@/stores/auth.store';
 import Separator from './ui/separator/Separator.vue';
 import Button from './ui/button/Button.vue';
 
-function logout() {
-  useAuthStore().signOut();
+async function logout() {
+  await useAuthStore().signOut();
 }
 </script>
 
 <template>
   <main>
     <div class="flex justify-between items-center pt-2 pr-2">
+      <RouterLink to="/dashboard">
       <div class="flex gap-4">
         <h1 class="text-2xl font-bold text-gray-800">⚡ Smart Gen</h1>
         <p class="text-gray-500 mt-1">Monitoramento de geradores de energia</p>
       </div>
+      </RouterLink>
 
       <div class="flex items-center gap-4">
         <RouterLink to="/">
           <Button @click="logout" variant="outline">Sair</Button>
         </RouterLink>
-
+<RouterLink to="/perfil">
         <img
           class="w-10 h-10 rounded-full shrink-0 object-cover border-2 border-gray-200"
-          src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp"
+          src="/images/user.svg"
           alt="User"
         />
+        </RouterLink>
       </div>
     </div>
     <Separator class="my-2"/>
