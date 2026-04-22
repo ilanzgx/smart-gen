2ad6fab46988b19081b4c66978f8248634a37829
GabrielVerwt <ryang6361@gmail.com>
2026-04-22 16:37:23 -0300

    style(website): melhorar página do erro 404 (closes #6)

--- apps/website/src/views/NotFoundView.vue
+++ apps/website/src/views/NotFoundView.vue
@@ -1,8 +1,14 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
 <template>
   <main class="min-h-screen flex items-center justify-center">
     <div class="text-center">
-      <h1 class="text-4xl">404</h1>
-      <h3 class="text-lg text-gray-500">Página não encontrada ou não existe</h3>
+      <h1 class="text-4xl mb-4">⚠️ ERRO 404</h1>
+      <h3 class="text-lg text-gray-500">Oops, parece que esta página não existe.</h3>
+      <h3 class="text-lg text-gray-500">Talvez você tenha digitado a URL errada ou a página foi removida.</h3>
+      <RouterLink to="/" class="text-blue-600 hover:underline mt-4 block">Voltar para a página inicial</RouterLink>
     </div>
   </main>
 </template>
