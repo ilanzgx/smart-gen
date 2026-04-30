a11c2f24de30ebaf5cf048f6a9bee9479bf6948d
ilanzgx <ilan-_@hotmail.com>
2026-04-30 18:52:05 -0300

    fix(firmware): inicializar o sensor de temperatura antes da primeira chamada para evitar retornar valores residuais

--- firmware/smartgen_sensors.cpp
+++ firmware/smartgen_sensors.cpp
@@ -1,12 +1,31 @@
 #include "smartgen_sensors.h"
 
+/**
+ * @brief Construtor da classe SmartGenSensors.
+ *
+ * @param oneWirePin - Pino digital onde o sensor DS18B20 está conectado.
+ */
 SmartGenSensors::SmartGenSensors(int oneWirePin) : _oneWirePin(oneWirePin), _oneWire(oneWirePin), _sensors(&_oneWire) {}
 
-void SmartGenSensors::init() 
+/**
+ * @brief Inicializa os sensores de temperatura DS18B20.
+ *
+ * @note O sensor DS18B20 precisa de um tempo para fazer a leitura inicial. Para evitar leituras residuais
+ * @return void
+ */
+void SmartGenSensors::init()
 {
   _sensors.begin();
+
+  _sensors.requestTemperatures();
+  delay(750);
 }
 
+/**
+ * @brief Lê a temperatura atual do sensor de temperatura (DS18B20).
+ *
+ * @return float - A temperatura atual em graus Celsius.
+ */
 float SmartGenSensors::getTemperature()
 {
   _sensors.requestTemperatures();
